1d0beef88924ef69a0f6475c2615f6b30361b949
janki1999 <78641274+janki1999@users.noreply.github.com>
2022-02-08 17:40:18 +0530

    package development done

---
+++ resources/views/vendor/password-change-notification/emails/password-changed-notification.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+This is a cutsomize view
+The password for your account has been changed.
+
+Please contact the administrator if you did not initiated this.
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
